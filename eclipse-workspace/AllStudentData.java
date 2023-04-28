67255350780db65c37024ed50422da5f6edde043
nejeet <131859883+nejeet@users.noreply.github.com>
2023-04-28 13:46:10 +0530

    Add files via upload

---
+++ eclipse-workspace/AllStudentData.java
@@ -0,0 +1,9 @@
+package com.model;
+
+public class AllStudentData
+{
+	public Student sa=new Student(10,"ytu","satara");
+	public Student sb=new Student(20,"hgu","nashik");
+	public Student sc=new Student(30,"wer","mumbai");	
+
+}
---
+++ eclipse-workspace/Student.java
@@ -0,0 +1,16 @@
+package com.model;
+
+public class Student
+{
+	public int rollno;
+	public String  name;
+	public String address;
+	public Student(int id,String sname,String saddress)
+	{
+		rollno=id;
+		name=sname;
+		address=saddress;
+	}
+	
+
+}
